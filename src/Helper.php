2ea58a07d4788359878c341ab1acfa4ba90d96fc
sajwarna <110798546+sajwarna@users.noreply.github.com>
2025-06-11 10:57:43 +1000

    Update to detect emojis in Helper.php

--- src/Helper.php
+++ src/Helper.php
@@ -56,7 +56,15 @@ class Helper
      */
     public static function hasUTFChars($message)
     {
-        return (bool)preg_match('/[А-Яа-яЁё]/u', $message);
+        $unicodeRegexp = '([*#0-9](?>\\xEF\\xB8\\x8F)?\\xE2\\x83\\xA3|\\xC2[\\xA9\\xAE]|\\xE2..(\\xF0\\x9F\\x8F[\\xBB-\\xBF])?(?>\\xEF\\xB8\\x8F)?|\\xE3(?>\\x80[\\xB0\\xBD]|\\x8A[\\x97\\x99])(?>\\xEF\\xB8\\x8F)?|\\xF0\\x9F(?>[\\x80-\\x86].(?>\\xEF\\xB8\\x8F)?|\\x87.\\xF0\\x9F\\x87.|..(\\xF0\\x9F\\x8F[\\xBB-\\xBF])?|(((?<zwj>\\xE2\\x80\\x8D)\\xE2\\x9D\\xA4\\xEF\\xB8\\x8F\k<zwj>\\xF0\\x9F..(\k<zwj>\\xF0\\x9F\\x91.)?|(\\xE2\\x80\\x8D\\xF0\\x9F\\x91.){2,3}))?))';
+        preg_match($unicodeRegexp, $message, $matches);
+       
+        if($matches) {
+            return true;
+        } else {
+            return false;
+        }
+        //return (bool)preg_match('/[А-Яа-яЁё]/u', $message);
     }
 
 }
